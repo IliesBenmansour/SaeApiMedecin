85aa85d781fc303e9b1fcf69ed3937bb2945bf3e
Marwan YOUMNI <95295521+Marwwannn@users.noreply.github.com>
2024-03-13 08:20:15 +0100

    a

--- proapi/back/auth/connexionDB.php
+++ proapi/back/auth/connexionDB.php
@@ -2,7 +2,7 @@
 // Connexion au serveur MySQL
 function connexionBD() {
 try {
-    $linkpdo = new PDO("mysql:host=localhost;dbname=r401_api;charset=utf8", "root", "");
+    $linkpdo = new PDO("mysql:host=localhost;dbname=projet_medecin;charset=utf8", "root", "");
     $linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $linkpdo;
 } catch (PDOException $e) {
